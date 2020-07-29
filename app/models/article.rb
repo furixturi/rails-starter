ae2c8c52f88abd6afaac0037f52a0ddc9585bb81
furixturi <furixturi@gmail.com>
2020-07-29 22:44:12 +0900

    delete comment

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,6 @@
 # $ rails db:migrate
 
 class Article < ApplicationRecord
-  has_many :comments
+  has_many :comments, dependent: :destroy
   validates :title, presence: true, length: {minimum: 5}
 end
